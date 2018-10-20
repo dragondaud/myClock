83bbe4f15b3bc4bce13985142768ff805e2a79f6
dragondaud <dragondaud@gmail.com>
2018-10-19 19:18:15 -0600

    fix zoom

--- WebServer.ino
+++ WebServer.ino
@@ -7,7 +7,7 @@
 
 static const char* serverHead PROGMEM =
   "<!DOCTYPE HTML><html><head>\n<title>myClock</title>\n"
-  "<meta name='viewport' content='width=device-width, initial-scale=.9'>"
+  "<meta name='viewport' content='width=device-width, initial-scale=.83'>"
   "<link href='stylesheet.css' rel='stylesheet' type='text/css'></head>\n"
   "<body><a href='https://github.com/dragondaud/myClock' target='_blank'>\n"
   "<h1>myClock " VERSION "</h1></a>\n";
