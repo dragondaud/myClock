a04f40a414a8905fa38b6afecbbe9ccebd193164
dragondaud <dragondaud@gmail.com>
2018-11-11 12:11:57 -0700

    show libs

--- update.sh
+++ update.sh
@@ -6,13 +6,13 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 ARDUINO=`realpath $SCRIPTPATH/..`
-cd $SCRIPTPATH && git pull --no-edit
-cd $ARDUINO/hardware/esp8266com/esp8266 && git pull --no-edit
-cd $ARDUINO/libraries/ArduinoJson && git pull --no-edit
-cd $ARDUINO/libraries/Syslog && git pull --no-edit
-cd $ARDUINO/libraries/Adafruit-GFX-Library && git pull --no-edit
-cd $ARDUINO/libraries/PxMatrix && git pull --no-edit
-cd $ARDUINO/libraries/WiFiManager && git checkout development && git pull --no-edit
-cd $ARDUINO/libraries/DallasTemperature && git pull --no-edit
-cd $ARDUINO/libraries/OneWire && git pull --no-edit
+cd $SCRIPTPATH  && echo -n "`basename $PWD`: " && git pull --no-edit
+cd $ARDUINO/hardware/esp8266com/esp8266 && echo -n "`basename $PWD`: " && git pull --no-edit
+cd $ARDUINO/libraries/ArduinoJson && echo -n "`basename $PWD`: " && git pull --no-edit
+cd $ARDUINO/libraries/Syslog && echo -n "`basename $PWD`: " && git pull --no-edit
+cd $ARDUINO/libraries/Adafruit-GFX-Library && echo -n "`basename $PWD`: " && git pull --no-edit
+cd $ARDUINO/libraries/PxMatrix && echo -n "`basename $PWD`: " && git pull --no-edit
+cd $ARDUINO/libraries/WiFiManager && echo -n "`basename $PWD`: " && git checkout development -q && git pull --no-edit
+cd $ARDUINO/libraries/DallasTemperature && echo -n "`basename $PWD`: " && git pull --no-edit
+cd $ARDUINO/libraries/OneWire && echo -n "`basename $PWD`: " && git pull --no-edit
 
