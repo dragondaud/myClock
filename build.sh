625a0ada00f9d33596632a5f51cd984fad75d6c9
dragondaud <26858302+dragondaud@users.noreply.github.com>
2019-04-11 12:15:51 -0600

    board manager tools, serial support

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ BIN="$SRC.bin"
 RM=`which rm`
 
 #default board d1_mini
-board="esp8266com:esp8266:d1_mini:xtal=160,vt=flash,eesz=4M1M,ip=lm2f,dbg=Disabled,lvl=NoAssert-NDEBUG,wipe=none,baud=921600"
+board="esp8266:esp8266:d1_mini:xtal=160,vt=flash,eesz=4M1M,ip=lm2f,dbg=Disabled,lvl=NoAssert-NDEBUG,wipe=none,baud=921600"
 port="8266"
 
-while getopts ":lvf:hcw" opt; do
+while getopts ":lvf:s:hcw" opt; do
 	case $opt in
 		l)
 			echo "Board lolin32 selected." >&2
@@ -27,15 +27,20 @@ while getopts ":lvf:hcw" opt; do
 			FLASH=$OPTARG
 			echo "Uploading to $FLASH after build." >&2
 			;;
+		s)
+			SER=$OPTARG
+			echo "Uploading to $SER after build." >&2
+			;;
 		h)
-			echo "Usage: build.sh [-l] [-v] [-f IP] [-h] [-c] [-w]"
+			echo "Usage: build.sh [-l] [-v] [-f IP] [-s /dev/ttyX|COMx] [-h] [-c] [-w]"
 			echo ""
 			echo "Build ${APP} and optionally flash device."
 			echo ""
 			echo "Options:"
 			echo -e "\t-l\tBuild for lolin32 instead of d1_mini"
 			echo -e "\t-v\tUse verbose output"
-			echo -e "\t-f\tIP address of ${APP} to flash update"
+			echo -e "\t-f\tIP address of ${APP} to update"
+			echo -e "\t-s\tSerial device of ${APP} to update"
 			echo -e "\t-h\tDisplay usage and exit"
 			echo -e "\t-c\tClean build directory before building"
 			echo -e "\t-w\tWipe entire flash, instead of sketch only"
@@ -58,22 +63,24 @@ done
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	arduino="/Applications/Arduino.app/Contents/MacOS/Arduino"
 	buildpath="$HOME/.build"
-	espota="$HOME/Documents/Arduino/hardware/esp8266com/esp8266/tools/espota.py"
-elif [[ "$OSTYPE" == "cygwin" ]]; then
+	arduinoLocal="`\"${arduino}\" --get-pref runtime.hardware.path 2>/dev/null`"
+	espota="`find \"${arduinoLocal}\" \"~/Documents/Arduino/hardware/\" \"~/Arduino\" -type f -name espota.py -print -quit`"
+	esptool="`find \"${arduinoLocal}\" \"~/Documents/Arduino/hardware/\" \"~/Arduino\" -type f -name esptool.py -print -quit`"
+elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
 	arduino="/c/Program Files (x86)/Arduino/arduino_debug.exe"
 	buildpath="${USERPROFILE}\.build"
-	espota="$HOME/Documents/Arduino/hardware/esp8266com/esp8266/tools/espota.py"
-elif [[ "$OSTYPE" == "msys" ]]; then
-	arduino="/c/Program Files (x86)/Arduino/arduino_debug.exe"
-	buildpath="${USERPROFILE}\.build"
-	espota="$HOME/Documents/Arduino/hardware/esp8266com/esp8266/tools/espota.py"
+	arduinoLocal="`\"${arduino}\" --get-pref runtime.hardware.path 2>/dev/null`"
+	espota="`find \"${arduinoLocal}\" \"$HOME/Documents/Arduino/hardware/\" -type f -name espota.py -print -quit`"
+	esptool="`find \"${arduinoLocal}\" \"$HOME/Documents/Arduino/hardware/\" -type f -name esptool.py -print -quit`"
 else
 	arduino="`which arduino`"
 	if [ -z "$arduino" ]; then
 		arduino="`find ~/.local /usr -type f -name arduino -print -quit`"
 	fi
 	buildpath="$HOME/.build"
-	espota="`find ~/Arduino/hardware/esp8266com ~/Documents ~/.local /usr -type f -name espota.py -print -quit`"
+	arduinoLocal="`\"${arduino}\" --get-pref runtime.hardware.path 2>/dev/null`"
+	espota="`find \"${arduinoLocal}\" \"~/Arduino/hardware/\" ~/Documents ~/.local /usr -type f -name espota.py -print -quit`"
+	esptool="`find \"${arduinoLocal}\" \"~/Arduino/hardware/\" ~/Documents ~/.local /usr -type f -name esptool.py -print -quit`"
 fi
 
 if [ -z "$arduino" ]; then
@@ -87,9 +94,15 @@ else
 	echo "Building ${APP} in ${buildpath}..." >&2
 	"${arduino}" ${verbose} --pref build.path="${buildpath}" --board "${board}" --save-prefs --verify ${SRC}
 	ret=$?
-	if [ $ret -eq 0 ] && [ -f "$espota" ] && [ ! -z "$FLASH" ]; then
-		echo "Flashing ${BIN} to ${FLASH}..." >&2
-		"${espota}" ${debug} --progress --file="${buildpath}/${BIN}" --ip=${FLASH} --port=${port}
+	if [ $ret -eq 0 ]; then
+		if [ -f "$espota" ] && [ ! -z "$FLASH" ]; then
+			echo "Flashing ${BIN} to ${FLASH}..." >&2
+			"${espota}" ${debug} --progress --file="${buildpath}/${BIN}" --ip=${FLASH} --port=${port}
+		elif [ -f "$esptool" ] && [ ! -z "$SER" ]; then
+			echo "Flashing ${BIN} to ${SER}..." >&2
+			"${esptool}" --port ${SER} --baud 921600 write_flash 0x0 "${buildpath}/${BIN}"
+		fi
+		echo "build complete"
 	else
 		exit $ret
 	fi
